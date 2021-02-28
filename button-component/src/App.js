e5f971a70ce6ac6a1e5bc8b1ff45ada54095efd5
may <sirinya.hwanphet@gmail.com>
2021-02-28 22:55:41 +0700

    l,.

---
+++ button-component/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Button from './Component/Button'
+function App() {
+  ReactDOM.render(
+    <div className="abc">
+      <Button  />
+     
+    </div>,
+    document.getElementById("root")
+  );
+}
+
+export default App;
