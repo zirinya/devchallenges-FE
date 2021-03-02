0730047f23002f9dc4bf515bac019c9fcdb501f6
may <sirinya.hwanphet@gmail.com>
2021-03-02 20:56:56 +0700

    .

--- button/src/components/button/Button.jsx
+++ button/src/components/button/Button.jsx
@@ -1,13 +1,11 @@
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+
+function button() {
+
+    return (
+      <div className="App">
+       dd
+      </div>
+    );
   }
   
-  const user = {
-    firstName: 'Harper',
-    lastName: 'Perez',
-  };
-  
-  const element = <h1>Hello, {formatName(user)}!</h1>;
-  
-  ReactDOM.render(element, document.getElementById('root'));
-  
+  export default button;
