3030b774cd891be1ba7796a53c45f48ff27572d6
may <sirinya.hwanphet@gmail.com>
2021-04-18 05:39:56 +0700

    .

--- button/src/App.vue
+++ button/src/App.vue
@@ -7,7 +7,7 @@
     <Button variant="text" disabled />
     <Button color="danger" />
     <Button color="secondary" />
-    <Button color="primary" text="ariaLabel"></Button>
+    <Button color="primary" ></Button>
   </div>
 </template>
 
